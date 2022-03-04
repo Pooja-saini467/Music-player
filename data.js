50bba7972ef3050fa1908f76e40e3f841a1356d6
kimnamjoon123 <poojasaini152018@gmail.com>
2022-03-05 02:23:37 +0530

    First Commit

---
+++ data.js
@@ -0,0 +1,38 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'Music/Song 1.mp3',
+        artist: 'BTS',
+        cover: 'images/cover 1.jpg'
+    },
+    {
+        name: 'song 2',
+        path: 'Music/Song 2.mp3',
+        artist: 'Dua Lipa',
+        cover: 'images/cover 2.jpg'
+    },
+    {
+        name: 'song 3',
+        path: 'Music/Song 3.mp3',
+        artist: 'BTS',
+        cover: 'images/cover 3.jpg'
+    },
+    {
+        name: 'song 4',
+        path: 'Music/Song 4.mp3',
+        artist: 'Taylor Swift',
+        cover: 'images/cover 4.jpg'
+    },
+    {
+        name: 'song 5',
+        path: 'Music/Song 5.mp3',
+        artist: 'Serena',
+        cover: 'images/cover 5.jpg'
+    },
+    {
+        name: 'song 6',
+        path: 'Music/Song 6.mp3',
+        artist: 'Taylor Swift',
+        cover: 'images/cover 6.png'
+    },
+]
